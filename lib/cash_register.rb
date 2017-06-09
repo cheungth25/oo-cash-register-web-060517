de6734b7f66af1d77e9a90429718b409e10ff66b
cheungth25 <cheungth25@gmail.com>
2017-06-09 00:23:37 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
-
+class CashRegister
+  attr_accessor :total
+  attr_reader :items, :discount
+  def initialize(employee_discount=0)
+    @total = 0
+    @discount = employee_discount
+    @items = []
+  end
+  def add_item(title, price, quantity=1)
+    @price = price
+    for i in 1..quantity
+      @total += price
+      @items << title
+    end
+  end
+  def apply_discount
+    return "There is no discount to apply." if @discount == 0
+    @total = @total * (100 - @discount) / 100
+    "After the discount, the total comes to $#{@total}."
+  end
+  def void_last_transaction
+    @total -= @price
+    @items.delete_at(-1)
+  end
+end
